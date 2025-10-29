c748d0339d8f3a9edf17fb6e49c74f7784864ba7
yoavboim <yoavboim1@gmail.com>
2025-10-29 14:28:50 +0200

    HM1

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,11 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
-	}
+    public static void main(String[] args) {
+        // To get you started, here is the first line in the program:
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];  
+        int amount = Integer.parseInt(args[3]);
+        double amountPerPerson = Math.ceil(amount / 3.0);
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + amountPerPerson + " Shekels each.");
+    }
 }
