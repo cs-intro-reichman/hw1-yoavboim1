db84fa653c5b1067723fb3d977fa6b161f63c355
yoavboim <yoavboim1@gmail.com>
2025-10-29 14:28:50 +0200

    HM1

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,9 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 =  args[2];  
+		int amount = Integer.parseInt(args[3]);
+		System.out.println("Dear " + name3 +", " + name2 + ", and " + name1+ ": pay " + amount + " Shekels each.");
 	}
 }
